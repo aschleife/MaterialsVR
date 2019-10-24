5f880c2ab78848eb58a30c64a99211d525b553cc
Luo, Zhili <zhilil2@illinois.edu>
2019-10-24 11:12:26 -0500

    2018.4.11f1

---
+++ Assets/Scripts/UICanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UICanvas : MonoBehaviour {
+
+    private Canvas canvas;
+
+    // Use this for initialization
+    void Start () {
+        canvas = GetComponent<Canvas>();
+        canvas.worldCamera = GameObject.FindGameObjectWithTag("UIRaycastCamera").GetComponent<Camera>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
